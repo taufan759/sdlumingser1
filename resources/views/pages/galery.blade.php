082a7bf4cd5e5a45d40d989ced1259881e7abd8c
VernandaFazira <vernandafazira@gmail.com>
2024-12-05 11:56:40 +0700

    Tampilan Gallery Sementara

--- resources/views/pages/galery.blade.php
+++ resources/views/pages/galery.blade.php
@@ -4,14 +4,16 @@
 <div class="container" style="margin-top: 75px; margin-bottom: 75px;">
     <div class="row">
         <div data-aos="fade-up" class="col text-center">
-            <h1>Galeri</h1>
-            <h2>SD Negeri Lumingser 01</h2>
+            <h1 class="gallery-title">GALERI</h1>
+            <div class="line-separator mx-auto"></div>
+            <h2 class="gallery-subtitle">SDN Lumingser 01</h2>
         </div>
     </div>
     
+    
     <section class="row">
         <section class="col-12">
-            {{-- Array untuk mengelompokkan berita berdasarkan kategori --}}
+            {{-- Foto Galeri --}}
             @php
                 $categories = [];
             @endphp
@@ -39,6 +41,37 @@
                 </div>
             @endforeach
 
+            {{-- Separator Line --}}
+            <hr class="gallery-separator">
+
+            <div class="section-title text-center mt-4">
+                <h3 class="youtube-title">Video YouTube</h3>
+                <div class="line-separator mx-auto"></div>
+            </div>
+
+            @php
+                $videos = [
+                    'https://www.youtube.com/embed/o85C9cqXvV8?si=W_XjYfreq-VawGUd',
+                    'https://www.youtube.com/embed/o85C9cqXvV8?si=W_XjYfreq-VawGUd',
+                    'https://www.youtube.com/embed/o85C9cqXvV8?si=W_XjYfreq-VawGUd'
+                ];
+            @endphp
+
+            <div class="row">
+                @foreach ($videos as $video)
+                    <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
+                        <iframe width="100%" height="200" src="{{ $video }}" frameborder="0" allowfullscreen></iframe>
+                    </div>
+                @endforeach
+            </div>
+
+            {{-- Tombol YouTube --}}
+            <div class="text-center mt-4">
+                <a href="https://www.youtube.com/@sdnegerilumingser0152" target="_blank" class="btn yt-custom">
+                    <i class="bi bi-youtube"></i>
+                    SDN LUMINGSER 01
+                </a>
+            </div>
         </section>
     </section>
 </div>
